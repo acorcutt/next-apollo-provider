bbd4444d0b5e69e75acea7fd433a081c70f7220c
Anthony Corcutt <acorcutt@me.com>
2017-04-12 13:35:25 +0000

    point to correct dist path again

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Point at something in .next/dist if you want an ES5 module
-module.exports = require('./next/dist/lib/next-apollo-provider');
+module.exports = require('./.next/dist/lib/next-apollo-provider');
